0aaf872af927d258506329c1393b22d067922bd4
Akash Vijaysingh Shekhavat <mycountryakash@gmail.com>
2023-02-11 08:29:04 +0530

    Reverse an array using XOR Swapping The time complexity is O(n).It is the most optimized solution to swap the array.

--- src/com/Akash/Main.java
+++ src/com/Akash/Main.java
@@ -1,8 +1,19 @@
 package com.Akash;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = {1,2,3,4,5,6};
+        int start =0;
+        int end = arr.length-1;
+        while(start<end){
+            arr[start] = arr[start]^arr[end];
+            arr[end] = arr[start]^arr[end];
+            arr[start] = arr[start]^arr[end];
+            start ++;
+            end --;
+        }
+        System.out.println(Arrays.toString(arr));
+
     }
 }
